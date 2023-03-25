11eea8efabee67c8a28a2f96a92d14f5901af1e2
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2023-03-25 11:25:38 +0100

    all updates

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('funguju!');
+
+const questionElm = document.querySelector('.question');
+questionElm.textContent =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+
+const odpoved = prompt(
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.',
+);
+
+const answerTextElm = document.querySelector('.answer__text');
+answerTextElm.textContent = odpoved;
+console.log(odpoved);
+
+const answerElm = document.querySelector('.answer');
+if (odpoved.toLowerCase() === 'bell') {
+  answerElm.classList.add('answer--correct');
+} else {
+  answerElm.classList.add('answer--wrong');
+}
